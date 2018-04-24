94bdc2ee0bdc9f0cd4cb940d97e4797874b68458
fanyanbo <fanyanbo@skyworth.com>
2018-04-24 10:08:59 +0800

    aa

--- models/contentModel.js
+++ models/contentModel.js
@@ -163,7 +163,8 @@ ContentModel.prototype.updateModuleData = function (engName, cnName, category, g
   // });
     let sql = "UPDATE modules SET cnName = ?, category = ?, gitPath = ?, descText = ?, orderId = ? WHERE engName = ?";
     logger.debug(sql);
-    let sql_params = [cnName,category,gitPath,desc,_orderId,engName];
+    let sql_params = [cnName,category,gitPath,desc,orderId,engName];
+    logger.debug(sql_params);
     db.conn.query(sql,sql_params,function(err,rows,fields){
       if (err) return callback(err);
       callback(null, rows);
