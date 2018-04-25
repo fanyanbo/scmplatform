7f5ee39d5a34b41a8aa4fd4e933d8a1215f72550
fanyanbo <fanyanbo@skyworth.com>
2018-04-25 18:15:19 +0800

    aa

--- models/syslogModel.js
+++ models/syslogModel.js
@@ -6,7 +6,8 @@ var logger = require('../common/logger');
 var SyslogModel = function() {};
 
 SyslogModel.prototype.queryByPage = function (offset, rows, callback) {
-  var sql = "select * from syslog order by time desc limit ?,?";
+//  var sql = "select * from syslog order by time desc limit ?,?";
+  let sql = "select userName,action,detail,FROM_UNIXTIME(time, '%Y-%m-%d %H:%i:%S') from syslog order by time desc limit ?,?";
   let sql_params = [offset,rows];
   db.conn.query(sql,sql_params,function(err,rows,fields){
     if (err) {
