ef56a8c18d0f5664d4d647c4d0e566bb763ca7d6
fanyanbo <fanyanbo@skyworth.com>
2018-04-25 18:28:14 +0800

    aa

--- controllers/home.js
+++ controllers/home.js
@@ -40,9 +40,6 @@ exports.querySyslog = function (req, res, next) {
       if(err) {
         return output.error(req,res,err);
       }
-      for(var i = 0; i < results.length; i++){
-        console.log(results[i]);
-      }
       output.success(req,res,"querySyslog查询成功",results);
     });
 };
