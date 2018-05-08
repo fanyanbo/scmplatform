c70ad2cd366d16ccf83eab5d3d5d2680644b8851
fanyanbo <fanyanbo@skyworth.com>
2018-05-08 19:09:50 +0800

    aaa

--- models/moduleModel.js
+++ models/moduleModel.js
@@ -109,7 +109,7 @@ ModuleModel.prototype.update = function (engName, cnName, category, gitPath, des
   let sql_order_param = [category];
   db.conn.query(sql_order,sql_order_param,function(err,rows,fields){
     if (err) return callback(err);
-    let _orderId = (rows.length == 0) ? 1 : rows[0].count + 1; //当新类别中没有任何模块是判断
+    let _orderId = (rows.length == 0) ? 1 : rows[0].orderId + 1; //当新类别中没有任何模块是判断
     logger.debug('updateModuleData _orderId = ' + _orderId);
     let sql = "UPDATE modules SET cnName = ?, category = ?, gitPath = ?, descText = ?, orderId = ? WHERE engName = ?";
     let sql_params = [cnName,category,gitPath,desc,_orderId,engName];
