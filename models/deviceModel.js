cd354cfb35fa51fa7dd3445cd868f94e5279f803
fanyanbo <fanyanbo@skyworth.com>
2018-05-09 19:56:33 +0800

    aaa

--- models/deviceModel.js
+++ models/deviceModel.js
@@ -119,10 +119,9 @@ DeviceModel.prototype.queryTargetProduct = function (callback) {
 /**
  * @param {新增targetproduct,这里需要更新两个表，mkdata和targetProducts}
  */
-DeviceModel.prototype.addTargetProduct = function (value, callback) {
+DeviceModel.prototype.addTargetProduct = function (name, arr, callback) {
 
   if(arr.length == 0) return callback("addTargetProduct参数错误！",null);
-
   let ep = new eventproxy();
 
   ep.bind('error', function (err) {
@@ -131,12 +130,12 @@ DeviceModel.prototype.addTargetProduct = function (value, callback) {
       callback(err,null);
   });
 
-  ep.after('insert_result', arr.length + 1, function (list) { // 注意：长度是arr.length + 1
+  ep.after('insert_result', arr.length + 1, function (list) { // 注意：长度是 arr.length + 1
       callback(null,"addTargetProduct OK");
   });
 
   let sql0 = "INSERT INTO targetProducts(name) values (?)";
-  let sql_params0 = [value];
+  let sql_params0 = [name];
   db.conn.query(sql0,sql_params0,function(err,rows,fields){
     if (err) return ep.emit('error', err);
     ep.emit('insert_result', 'sql0 ok');
@@ -144,7 +143,7 @@ DeviceModel.prototype.addTargetProduct = function (value, callback) {
 
   for (let i = 0; i < arr.length; i++) { //数据结果与调用顺序无关
     let sql = "INSERT INTO mkdata（targetProduct,engName) values(?,?)";
-    let sql_params = [arr[i].targetproduct,arr[i].engName];
+    let sql_params = [name ,arr[i].engName];
     db.conn.query(sql,sql_params,function(err,rows,fields) {
       if (err) return ep.emit('error', err);
       console.log('insert_result----ok' + i);
