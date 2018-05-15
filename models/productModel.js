d93f5da36badff6a4499f60a96b207bf3d68dd3a
fanyanbo <fanyanbo@skyworth.com>
2018-05-15 16:49:16 +0800

    aa

--- models/productModel.js
+++ models/productModel.js
@@ -285,12 +285,9 @@ ProductModel.prototype.addHistory = function (data, callback) {
   console.log(data);
   console.log(data.chip);
   console.log(data.model);
-  let dataObj = JSON.parse(data);
-  console.log(dataObj.chip);
-  console.log(dataObj.model);
 
   let sql = "INSERT INTO modifyhistory(chip,model,state,userName,content,reason) values (?,?,?,?,?,?)";
-  let sql_param = [dataObj.chip,dataObj.model,dataObj.state,dataObj.userName,dataObj.content,dataObj.reason];
+  let sql_param = [data.chip,data.model,data.state,data.userName,data.content,data.reason];
   console.log(sql_param);
   db.conn.query(sql,sql_param,function(err,rows,fields){
     if (err) return callback(err);
