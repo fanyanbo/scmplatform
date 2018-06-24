1c2ea9efd53e32bb56a6d8c5e914a276bc31eb64
liangquanqing <liangquanqing@skyworth.com>
2018-06-25 00:17:43 +0800

    插入 setting/prop 数据

--- tools/sql/v60同步到v62.sql
+++ tools/sql/v60同步到v62.sql
@@ -2,7 +2,18 @@ insert into v62_products (chip, model, memorySize, EMMC, targetProduct, soc, git
 	select chip, model, memorySize, EMMC, targetProduct, soc, gitBranch from v60_products;
 
 
+insert into v60_propsdata (targetProduct, engName, curValue)
+	select b.name, a.engName, a.defaultValue from props as a cross join targetProducts as b;
+insert into v62_propsdata (targetProduct, engName, curValue)
+	select b.name, a.engName, a.defaultValue from props as a cross join targetProducts as b;
 
+	
+insert into v60_settingsdata (chip, model, engName)
+	select b.chip, b.model, a.engName from v60_settings as a cross join v60_products as b where a.defaultSelected = 1;
+insert into v62_settingsdata (chip, model, engName)
+	select b.chip, b.model, a.engName from v62_settings as a cross join v62_products as b where a.defaultSelected = 1;
 
-
-
+	
+	
+	
+	
